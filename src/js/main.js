f0d5d28aa99032c40676225824470773ab13bee0
evamilos1 <linnlloyd@rambler.ru>
2019-08-31 23:58:02 +0400

    dz 7

--- src/js/main.js
+++ src/js/main.js
@@ -1,37 +1,81 @@
-//6.1 & 6.2
-var button = document.getElementsByClassName("change-block__elem")[0];
-    
-button.onclick = function push () {
-    if (document.getElementsByClassName("change-block__elem")[0]) {
-      document.getElementsByClassName("change-block__elem")[0].style.backgroundColor = "blue";
-      document.getElementsByClassName("change-block__elem")[0].style.width = "300px";
-      document.getElementsByClassName("change-block__elem")[0].style.color = "white";
-      document.getElementsByClassName("change-block__elem")[0].value = ":)";
-      var div = document.createElement("div");
-          div.className = "new";
-          document.body.append(div);
-      document.getElementsByClassName("new")[0].style.width = "150px";
-      document.getElementsByClassName("new")[0].style.height = "150px";
-      document.getElementsByClassName("new")[0].style.background = "url(https://cs4.pikabu.ru/images/big_size_comm/2015-01_3/14211988902791.jpg)";
-      document.getElementsByClassName("new")[0].style.backgroundSize = "contain";
-      document.getElementsByClassName("new")[0].style.backgroundRepeat = "no-repeat";
-      document.getElementsByClassName("new")[0].style.transform = "translate(340%, -130%)";
-      document.getElementsByClassName("new")[0].style.borderRadius = "70px";
-    } else {
-      alert("Error!"); 
-    }
-  }
+//7.1
+var button = document.querySelector(".enter__action__button_js"),
+    mean = document.querySelector(".enter__action_js"),
+    add = document.querySelector(".table");
 
-function change () {
-  if (document.getElementsByClassName("change-block")[0]) {
-    document.getElementsByClassName("change-block")[0].style.width = "1180px";
-    document.getElementsByClassName("change-block")[0].style.backgroundColor = "green";
-  } else {
-    alert("Error!"); 
-  }
-}
+button.addEventListener('click', function() {
 
-document.getElementsByClassName("change-block")[0].addEventListener("click", change);
+    // добавляем элементы таблицы
+    var enter_tr = document.createElement('tr');
+    var enter_td = document.createElement('td');
+    // добавляем элемент div
+    var enter = document.createElement('div');
+    enter.className = 'business_js';
+    enter.innerHTML = '<p id="mean">' + mean.value + '</p>';
+    enter.classList.add('add-table-item_js');
+    // добавляем кнопки удалить и редактировать каждому новому элементу списка
+    var btn_delete = document.createElement('div');
+    var btn_change = document.createElement('div');
+    btn_delete.className = 'btn-delete_js';
+    btn_change.className = 'btn-change_js';
+
+    // функция удаления по нажатию на кнопку
+    btn_delete.addEventListener('click', function(event) {
+        var a = event.target;
+        var b = a.parentElement;
+        var c = b.parentElement;
+        var d = c.parentElement;
+        d.remove();
+    })
+
+    //функция редактирования по нажатию на кнопку
+    btn_change.addEventListener('click', function(event) {
+        var change_inp = document.createElement('input');
+        var change_ok = document.createElement('div');
+        change_ok.className = 'change-ok_js';
+        change_inp.className = 'change-inp_js';
+        change_ok.innerText = 'OK';
+        change_inp.value = 'Менять планы не хорошо! Но один раз прощаю.';
+
+        var a = event.target;
+        var b = a.parentElement;
+
+        b.append(change_inp);
+        b.append(change_ok);
+
+        change_ok.addEventListener('click', function (event) {
+            enter.innerText = change_inp.value;
+            var btn_delete = document.createElement('div');
+            btn_delete.className = 'btn-delete-new_js';
+
+            btn_delete.addEventListener('click', function(event) {
+                var a = event.target;
+                var b = a.parentElement;
+                var c = b.parentElement;
+                var d = c.parentElement;
+                d.remove();
+            })
+
+            enter.append(btn_delete);
+        })
+    })
+
+    //расположение основных созданных элементов на странице
+    enter.append(btn_delete);
+    enter.append(btn_change);
+    enter_td.append(enter);
+    enter_tr.append(enter_td);
+    add.append(enter_tr);
+});
 
 
+
+//7.2
+var temp = document.getElementsByClassName('temperature__enter__mean_js')[0];
+
+temp.oninput = function () {
+    var res = document.getElementsByClassName('temperature__enter__res_js')[0];
+    res.value = ((temp.value * (9/5)) + 32) + "°F";
+    return res;
+};
 
